bd898a198d9f2d914dba05ff3c97874852c70d2d
Ricky dildy <rdildy92@gmail.com>
2019-10-15 23:05:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,3 +4,6 @@ describe 'Dog' do
       fido = Dog.new("Fido", "Pug")
       expect(fido.instance_variable_get(:name)).to eq("Fido")
     end
+    it 'sets the breed of the dog in an instance variable @breed' do 
+      fido = Dog.new("Fido", "Pug")
+      
